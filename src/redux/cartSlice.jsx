1374093ddb89ee160ce049ea3b5699938d7722c1
AbdelazizAabbour <aabbourabdealziz@gmail.com>
2025-12-16 16:33:46 +0100

    add des comment

--- src/redux/cartSlice.jsx
+++ src/redux/cartSlice.jsx
@@ -4,12 +4,18 @@ const cartSlice = createSlice({
   name: "cart",
   initialState: { items: [] },
   reducers: {
+
+    // hadi bach n zido product l cart
     addToCart: (state, action) => {
       state.items.push(action.payload);
     },
+
+    // hadi bach n diro remove l product mn cart
     removeFromCart: (state, action) => {
       state.items = state.items.filter(i => i.id !== action.payload);
     },
+    
+    // hadi bach n diro update l quantity
     updateQty: (state, action) => {
       const item = state.items.find(i => i.id === action.payload.id);
       if (item) item.qty = action.payload.qty;
